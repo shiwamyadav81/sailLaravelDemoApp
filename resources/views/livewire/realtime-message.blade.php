fa6e2f9b2bb9d746c1bc6110d171b05fddccfb91
shiwam <shiwam.mvteams@gmail.com>
2024-03-28 11:15:10 +0530

    ui improvement

--- resources/views/livewire/realtime-message.blade.php
+++ resources/views/livewire/realtime-message.blade.php
@@ -1,15 +1,15 @@
 <div class="bg-white shadow-lg rounded-lg overflow-hidden">
     <div class="px-4 py-4 bg-gray-100">
-        <h2 class="text-xl font-semibold text-gray-800">Real Time Notifications</h2>
+        <h2 class="text-xl font-semibold text-gray-800">Notification List of Users Getting Registered</h2>
     </div>
     <div class="p-4">
-        @foreach ($uNotifications as $unotification)
-            <div class="py-2">
-                <p class="text-gray-700">New User Created: <apan class="font-bold text-cyan-400">
-                        {{ $unotification }}
-                </p>
+        @foreach ($uNotifications as $notification)
+            <div class="flex items-center py-2">
+                <img class="h-8 w-8 mr-2" src="{{ asset('images/user.png') }}" alt="User avatar">
+                <p class="text-cyan-600 font-bold">{{ $notification }}</p>
             </div>
             <hr class="border-gray-300">
         @endforeach
     </div>
+
 </div>
